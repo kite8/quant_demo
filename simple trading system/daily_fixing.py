3db3601616dc15cf4f91cce141019e02bdc84a66
kite8 <996845018@qq.com>
2018-10-15 00:08:35 +0800

    add files

---
+++ simple trading system/daily_fixing.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 19:52:31 2018
+
+@author: kite
+"""
+import datetime
+from pymongo import UpdateOne, ASCENDING
+from database import DB_CONN
