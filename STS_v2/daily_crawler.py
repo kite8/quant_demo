30c91b9847ef5d7e7266d053f039cdbf734e55c4
996845018@qq.com <Hao54321>
2018-11-01 18:24:36 +0800

    add

--- STS_v2/daily_crawler.py
+++ STS_v2/daily_crawler.py
@@ -7,8 +7,9 @@ Created on Sun Oct 14 13:12:41 2018
 from pymongo import MongoClient, ASCENDING, DESCENDING, UpdateOne
 from database import DB_CONN
 import tushare as ts
-import datetime
-
+import pandas as pd
+import datetime, time
+import QUANTAXIS as QA
 
 """
 从tushare获取日k数据，保存到本地的MongoDB数据库中
@@ -36,16 +37,47 @@ class DailyCrawler:
         start = str(start)[0:10]
         end = str(end)[0:10]
             
-        if type(code) == str:
-            df_daily = ts.get_k_data(code, start, end, index=True)
-            self.save_data(code, df_daily, self.daily, {'index':True})
+#        if type(code) == str:
+#            df_daily = ts.get_k_data(code, start, end, index=True)
+#        df = QA.QA_fetch_index_day_adv(code, start, end).data
+#        df = df.reset_index()
+#        df.date = df.date.apply(lambda x : x.strftime('%Y-%m-%d'))
+#        self.save_data(code, df, self.daily, {'index':True})
         
-        else:
-            total = len(code)
-            for i, c in enumerate(code):
-                df_daily = ts.get_k_data(c, start, end, index=True)
-                self.save_data(c, df_daily, self.daily, {'index':True})
-                print('index日线数据获取进度: (%s/%s)' % (i+1, total))
+#        else:
+        total = len(code)
+        for i, c in enumerate(code):
+            df = QA.QA_fetch_index_day_adv(c, start, end).data
+            df = df.reset_index()
+            df.date = df.date.apply(lambda x : x.strftime('%Y-%m-%d'))
+            self.save_data(c, df, self.daily, {'index':True})
+            print('index日线数据获取进度: (%s/%s)' % (i+1, total))
+        
+    def crawl(self, code, start, end=None):
+        """
+        
+        """
+        end = start if end is None else end
+        start = str(start)[0:10]
+        end = str(end)[0:10]
+        
+        total = len(code)
+        for i, c in enumerate(code):
+            try:
+                data = QA.QA_fetch_stock_day_adv(c, start, end)
+                df = data.data
+                df = df.reset_index()
+                df.date = df.date.apply(lambda x : x.strftime('%Y-%m-%d'))
+                self.save_data(c, df, self.daily, {'index':False})
+                
+                df_hfq = data.to_hfq().data
+                df_hfq = df_hfq.reset_index()
+                df_hfq.date = df_hfq.date.apply(lambda x : x.strftime('%Y-%m-%d'))
+                self.save_data(c, df_hfq, self.daily_hfq, {'index':False})
+            except:
+                err_code.append(c)
+                print('code : %s, 没有数据，请核实' % c)
+            print('股票日线数据获取进度: (%s/%s)' % (i+1, total))
                     
     def save_data(self, code, df_daily, collection, extra_fields=None):
         """
@@ -53,9 +85,9 @@ class DailyCrawler:
         """
         update_requests = []
         
-        for df_index in df_daily.index:
-            daily_obj = df_daily.loc[df_index]
-            doc = self.daily_obj_2_doc(code, daily_obj)
+        for i in df_daily.index:
+            doc = dict(df_daily.loc[i, ['code', 'date', 'open', 'high', 'low', 'close','volume',
+                      ]])
             
             if extra_fields is not None:
                 doc.update(extra_fields)
@@ -73,46 +105,27 @@ class DailyCrawler:
               (code, update_result.upserted_count, update_result.modified_count),
               flush=True)
     
-    def crawl(self, code, start, end=None):
-        """
-        
-        """
-        end = start if end is None else end
-        start = str(start)[0:10]
-        end = str(end)[0:10]
-        
-        total = len(code)
-        for i, c in enumerate(code):
-            df_daily = ts.get_k_data(c, start, end, autype=None)
-            self.save_data(c, df_daily, self.daily, {'index':False})
+
             
-            df_daily_hfq = ts.get_k_data(c, start, end, autype='hfq')
-            self.save_data(c, df_daily_hfq, self.daily_hfq, {'index':False})
-            print('股票日线数据获取进度: (%s/%s)' % (i+1, total))
-    
-    @staticmethod
-    def daily_obj_2_doc(code, daily_obj):
-        return {
-            'code': code,
-            'date': daily_obj['date'],
-            'close': daily_obj['close'],
-            'open': daily_obj['open'],
-            'high': daily_obj['high'],
-            'low': daily_obj['low'],
-            'volume': daily_obj['volume']
-        }    
-        
 if __name__ == '__main__':
     dc = DailyCrawler()
     
     index_code_list = ['000001', '000300', '399001', '399005', '399006']
-    stock_code_list = ts.get_stock_basics().index.tolist()
+    _df = pd.read_excel('data/stock_basic.xlsx', header=0, dtype={'code':str})
+    stock_code_list = _df.code.tolist()
 #    stock_code_list = ['000001', '000300', '399001', '399005', '399006']
     
     start = '2015-01-01'
     end = '2018-09-30'
+    err_code = []
+    tic = time.process_time()
     dc.crawl_index(index_code_list, start, end)
     dc.crawl(stock_code_list, start, end)
+    
+    toc = time.process_time()
+    print('cost time : %.2fs' % (toc-tic))
+    print(err_code)
+
     
     
     
